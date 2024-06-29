9e9b0163d993be9801acd76c0e383da1bcbbd2f0
Севентин глак <k1gdegor3485@gmail.com>
2024-06-29 20:56:35 +0300

    house

---
+++ house.py
@@ -0,0 +1,25 @@
+from random import randint
+import json
+
+SIDES = [1, 2, 3, 4, 5, 6]
+
+class Dice:
+    def __init__(self):
+        self.current_side = SIDES[randint(0, 5)]
+
+    def __repr__(self):
+        return str(self.current_side)
+
+    def roll(self):
+        self.current_side = SIDES[randint(0, 5)]
+
+    def save(self):
+        with open('data1', 'w') as file:
+            json.dump({"current_side": self.current_side}, file)
+            return {"current_side": self.current_side}
+
+    @staticmethod
+    def load():
+        with open('data1', 'r') as file:
+            s = json.load(file)
+        return s["current_side"]
