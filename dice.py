d70c70d46bf762f9447efbea53aa946f1ed4fb24
savva163 <k1gdegor3485@gmail.com>
2024-06-29 20:57:26 +0300

    Update dice.py

--- dice.py
+++ dice.py
@@ -11,22 +11,3 @@ c1 = Dice()
 c2 = Dice()
 c3 = Dice()
 print(c1, c1.roll())
-
-
-
-# house
-from scr.house import House
-class Player():
-    def init(self, name, House, PlayerInteraction, Dice):
-        self.name = name
-        self.house = House """"(class?)"""
-        self.type = PlayerInteraction"""(class?)"""
-        self.chosen_dice = Dice """(class?)"""
-    def __repr__(self):
-        pass
-
-    def chosen_dice(self):
-        pass
-
-    def chosen_action(self):
-        pass
