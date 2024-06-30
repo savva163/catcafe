3f77671d945d9e9bff15f24752b897b98c1e7d30
Севентин глак <k1gdegor3485@gmail.com>
2024-06-30 21:37:46 +0300

    player

--- player.py
+++ player.py
@@ -1,12 +1,12 @@
 from scr.house import House
 class Player():
-def __init__(self, name, house, interaction, dice):
-    self.name = name
-    self.house = House
-    self.interaction = interaction
-    self.dice = dice
-def choose_dice(self):
-    pass
-def choose_action(self):
-    pass
+    def __init__(self, name, house, interaction, dice):
+        self.name = name
+        self.house = House
+        self.interaction = interaction
+        self.dice = dice
+    def choose_dice(self):
+        pass
+    def choose_action(self):
+        pass
 
