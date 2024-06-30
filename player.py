fe91b2a232fc6c06d6ee05aa255a56c5725bedc3
Севентин глак <k1gdegor3485@gmail.com>
2024-06-30 21:32:49 +0300

    house

---
+++ player.py
@@ -0,0 +1,11 @@
+from scr.house import House
+class Player():
+def init(self, name, house, interaction, dice):
+self.name = name
+self.house = House
+self.interaction = interaction
+self.dice = dice
+def choose_dice(self):
+    pass
+def choose_action(self):
+    pass
