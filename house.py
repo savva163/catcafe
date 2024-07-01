1f4385e8ac6435f952c2ad222006079d6c054086
Севентин глак <k1gdegor3485@gmail.com>
2024-07-01 08:44:03 +0300

    player

--- house.py
+++ house.py
@@ -12,28 +12,23 @@ class House:
                       ["", "", "", "", "", ""],
                       ["", "", "", "", "", ""]]
 
-    def item(self, i, g):
-        item = ITEMS[self.house[i][g] - 1] if isinstance(self.house[i][g], int) else "_______"
-        return item
-
     def __repr__(self):
-        return f"""  
-                             7/3
-                 9/5          |           8/4
-   6/4           |         6_{self.item(0, 2)}_      |
-    |         6_{self.item(0, 1)}_      |         6_{self.item(0, 3)}_
- 5_{self.item(1, 0)}_      |         5_{self.item(1, 2)}_      |
-    |         5_{self.item(1, 1)}_      |         5_{self.item(1, 3)}_
-    |            |         4_{self.item(2, 2)}_      |           3/2
-    |         4_{self.item(2, 1)}_      |         4_{self.item(2, 3)}_      |
- 3_{self.item(3, 0)}_      |            |            |         3_{self.item(3, 4)}_
-    |         3_{self.item(3, 1)}_      |         3_{self.item(3, 3)}_      |
- 2_{self.item(4, 0)}_      |         2_{self.item(4, 2)}_      |         2_{self.item(4, 4)}_
-    |         2_{self.item(4, 1)}_      |         2_{self.item(4, 3)}_      |
- 1_{self.item(5, 0)}_      |         1_{self.item(5, 2)}_      |         1_{self.item(5, 4)}_
-    |         1_{self.item(5, 1)}_      |         1_{self.item(5, 3)}_      |
-    |            |            |            |            |
-    =            =            =            =            ="""
+        return f"""   7/3
+                 9/5   |                     8/4
+            6/4   |   6_{self.house[0][2]}_   |
+             |   6_{self.house[0][1]}_   |   6_{self.house[0][3]}_
+            5_{self.house[1][0]}_  |   5_{self.house[1][2]}_   |
+             |   5_{self.house[1][1]}_   |   5_{self.house[1][3]}_
+             |    |   4_{self.house[2][2]}_   |                     3/2
+             |   4_{self.house[2][1]}_   |   4_{self.house[2][3]}_   |
+            3_{self.house[3][0]}_   |    |    |   3_{self.house[3][4]}_
+             |   3_{self.house[3][1]}_   |   3_{self.house[3][3]}_   |
+            2_{self.house[4][0]}_   |   2_{self.house[4][2]}_   |   2_{self.house[4][4]}_
+             |   2_{self.house[4][1]}_   |   2_{self.house[4][3]}_   |
+            1_{self.house[5][0]}_   |   1_{self.house[5][2]}_   |   1_{self.house[5][4]}_
+             |   1_{self.house[5][1]}_   |   1_{self.house[5][3]}_   |
+             |    |    |    |    |
+             =    =    =    =    ="""
 
     def score(self):
         s = sum([self.score_1(), self.score_2(), self.score_3(),
@@ -78,6 +73,7 @@ class House:
 
     def load(self):
         pass
+
 
 
 
