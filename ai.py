40b980f428bb621bf0fe1d27a1eb4e952db49cbf
Севентин глак <k1gdegor3485@gmail.com>
2024-07-04 02:09:27 +0300

    player

---
+++ ai.py
@@ -0,0 +1,12 @@
+from random import randint
+from random import choice
+from house import House
+
+items = [1, 2, 3, 4, 5, 6]
+
+class AI():
+    def choose_dice(self):
+        pass
+
+    def choose_action(self):
+        pass
