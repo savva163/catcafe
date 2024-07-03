9aeb15b5ca1a451cdbb45fc2404f220ac9771f20
Севентин глак <k1gdegor3485@gmail.com>
2024-07-04 01:52:27 +0300

    player

--- dice.py
+++ dice.py
@@ -1,22 +1,29 @@
 import json
-from random import choice
+from random import randint
 
-SIDES = [1, 2, 3, 4, 5, 6]
-class Dice:
-    def __init__(self):
-        self.current_side = choice(SIDES)
-    def __repr__(self):
+class Dice():
+    SIDES = [1, 2, 3, 4, 5, 6]
+
+    def init(self, side=None):
+        if side and 1 <= side <= 6:
+            self.current_side = side
+        else:
+            self.roll()
+
+    def repr(self):
         return str(self.current_side)
+
     def roll(self):
-        self.current_side = choice(SIDES)
+        self.current_side = self.SIDES[randint(0, 5)]
+
     def save(self):
-        with open('data1.json', 'w') as file:
-            json.dump({"current_side": self.current_side}, file)
-            return {"current_side": self.current_side}
-    @staticmethod
-    def load():
-        with open('data1.json', 'r') as file:
-            data = json.load(file)
-        return data["current_side"]
+        return repr(self)
+
+    @classmethod
+    def load(cls, value: str):
+        return cls(value)
+
+
+
 
 
