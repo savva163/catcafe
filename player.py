2e8c83cffce6ec22a49432a154fb60cd1f11a13b
Севентин глак <k1gdegor3485@gmail.com>
2024-07-05 02:14:39 +0300

    player

--- player.py
+++ player.py
@@ -1,27 +1,22 @@
 from ai import AI
-from dice import Dice
 from house import House
 from human import Human
 
-DICES = [Dice(), Dice(), Dice()]
-
-class Player:
-    def __init__(self, name: str, house: House = None, is_human: bool = False):
+class Player():
+    def init(self, name: str, house: House = None, dice = None, is_human: bool = False):
         self.name = name
-        self.tower_lst = house
-        if is_human:
-            self.actor = Human()
-        else:
-            self.actor = AI()
-            
+        self.tower_lst = house if house is not None else House()
+        self.choosen_dice = dice
+        self.actor = Human() if is_human else AI()
+
     def __repr__(self):
         return f'{self.name}: {self.tower_lst}'
 
     def choose_dice(self, dices):
         return self.actor.choose_dice(dices)
 
-    def choose_action(self):
-        return self.actor.choose_action(self.tower_lst, DICES)
+    def choose_action(self, dices=[int, int]):
+        return self.actor.choose_action(self.tower_lst, dices)
 
     def to_dict(self):
         return {
@@ -32,7 +27,7 @@ class Player:
 
     @classmethod
     def from_dict(cls, data: dict):
-        return cls(data["name"], House(data["house"]), data["is_human"])
+        return cls(data["name"], data["house"], data["is_human"])
 
     def save(self):
         return self.to_dict()
@@ -40,9 +35,3 @@ class Player:
     @classmethod
     def load(cls, data: dict):
         return cls.from_dict(data)
-
-if __name__ == '__main__':
-    print(type(DICES[0]))
-
-
-
