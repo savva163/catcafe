320c5aeb130d97ae66f0dbaa32d3d8a3c3cd6f57
Севентин глак <k1gdegor3485@gmail.com>
2024-07-05 01:52:28 +0300

    player

--- house_test.py
+++ house_test.py
@@ -1,10 +1,13 @@
-from house import (House)
+from house import House
 
 def test_position():
     c = House()
+    assert c._position(2, 3) == True
     assert c._position(12, 3) == False
     assert c._position(2, -1) == False
+    assert c._position(12, 12323) == False
     assert c._position(-1, -3) == False
+
 
 def test_init():
     c = House()
@@ -32,33 +35,63 @@ def test_repr():
 def test_put():
     c = House()
     c.put(3, 3, 1)
-    assert c.house[7][2] == "NAN"
+    assert c.house[6][2] == "NAN"
     c.put(3, 2, 2)
-    assert c.house[9][2] == 2
+    assert c.house[8][2] == 2
+    c.put(2, 6, 5)
+    assert c.house[1][1] == 5
+    print(c)
 
+def test_get():
+    c = House()
+    c.put(3, 3, 1)
+    assert c._get(3, 3) == "NAN"
+    c.put(3, 2, 2)
+    assert c._get(3, 2) == 2
+    c.put(2, 6, 5)
+    assert c._get(2, 6) == 5
+    print(c)
 
 def test_score():
     c = House()
     c.put(1, 1, 1)
     c.put(1, 2, 2)
+    c.put(1, 3, 3)
     c.put(1, 5, 5)
-def test_score_home1():
+    c.put(2, 2, 4)
+    c.put(2, 1, 6)
+    c.put(3, 1, 6)
+    c.put(4, 2, 6)
+    c.put(3, 2, 4)
+    c.put(2, 3, 2)
+    c.put(2, 5, 5)
+    c.put(2, 4, 3)
+    c.put(2, 6, 2)
+    c.put(5, 1, 1)
+    c.put(5, 3, 5)
+    c.put(4, 3, 6)
+    c.put(4, 4, 6)
+    print(c)
+    assert c.score() == 60
+def test_score_home_1():
     a = House()
-    assert a.score_home1() == 0
+    assert a.score_home_1() == 0
     a.put(1, 1, 2)
-    assert a.score_home1() == 0
+    assert a.score_home_1() == 0
     a.put(1, 2, 2)
     a.put(1, 3, 2)
     a.put(1, 5, 2)
-    assert a.score_home1() == 4
+    assert a.score_home_1() == 4
 
 def test_score_ball_2():
     c = House()
+    c.put(1, 2, 2)
     assert c.score_ball_2() == 0
     c.put(5, 1, 2)
     assert c.score_ball_2() == 3
     c.put(5, 3, 2)
     assert c.score_ball_2() == 8
+    print(c)
 
 def test_score_butterfly_3():
     c = House()
@@ -72,18 +105,28 @@ def test_score_butterfly_3():
 
 def test_score_bowl_4():
     c = House()
-    assert c.score_bowl_4() == 1
-    c.put(3, 4, 2 )
+    c.put(3, 4, 2)
     assert c.score_bowl_4() == 2
     c.put(2, 5, 3)
+    assert c.score_bowl_4() == 3
+    c.put(2, 6, 5)
     assert c.score_bowl_4() == 4
     c.put(3, 6, 6)
+    assert c.score_bowl_4() == 5
+    print(c)
+
+def test_score_bowl():
+    c = House()
+    c.put(4, 5, 4)
+    assert c.score_bowl() == 3
+    c.put(4, 6, 4)
+    assert c.score_bowl() == 5
+    c.put(3, 6, 4)
+    assert c.score_bowl() == 7
     print(c)
 
 def test_score_pillow_5():
     c = House()
-    c.put(4, 5, 5)
-    assert c.score_pillow_5() == 15
     c.put(3, 6, 5)
     assert c.score_pillow_5() == 21
     c.put(1, 2, 5)
@@ -91,29 +134,37 @@ def test_score_pillow_5():
     c.put(1, 3, 5)
     assert c.score_pillow_5() == 26
     c.put(1, 5, 5)
-    assert c.score_5() == 31
+    assert c.score_pillow_5() == 31
     print(c)
 def test_score_6():
     c = House()
+    c.put(3, 1, 6)
+    assert c.score_mouse_6() == 14
+    c.put(4, 1, 6)
+    assert c.score_mouse_6() == 22
     c.put(5, 1, 6)
     assert c.score_mouse_6() == 22
     c.put(5, 2, 6)
     assert c.score_mouse_6() == 22
-    c.put(2, 4, 6)
-    assert c.score_mouse_6() == 26
-    c.put(2, 5, 6)
-    assert c.score_mouse_6() == 32
     print(c)
 
 def test_save():
-    pass
+    c = House()
+    assert c.save() == c.house
 
 def test_load():
     c = House()
-    c.put(3, 4, 1)
-    print(c)
-    c.save()
-    a = House()
-    print(a)
-    a.load(c.save())
-    print(a)
+    c.put(3,2,1)
+    c = c.load(c.house)
+    assert c.house == [["NAN", "NAN", "", "NAN", "NAN"],
+                      ["NAN", "", "NAN", "", "NAN"],
+                      ["", "NAN", "", "NAN", "NAN"],
+                      ["NAN", "", "NAN", "", "NAN"],
+                      ["NAN", "NAN", "", "NAN", "NAN"],
+                      ["NAN", "", "NAN", "", "NAN"],
+                      ["", "NAN", "NAN", "NAN", ""],
+                      ["NAN", "", "NAN", "", "NAN"],
+                      ["", "NAN", 1, "NAN", ""],
+                      ["NAN", "", "NAN", "", "NAN"],
+                      ["", "NAN", "", "NAN", ""],
+                      ["NAN", "", "NAN", "", "NAN"]]
